059a062c7d2457fe4a9c374513fef10210432858
Gajjar Dhriti <dhriti.g1012@gmail.com>
2026-03-01 10:41:12 +0530

    Add files via upload

---
+++ basic-calculation.js
@@ -0,0 +1,68 @@
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+console.log("1:", addTwoNumbers(25, 35));
+// 2
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+console.log("2:", num1 + num2 + num3);
+// 3
+var grandTotal = 150 + 250;
+console.log("3:", grandTotal);
+// 4
+var score = 80;
+score = score + 10;
+console.log("4:", score);
+// 5
+console.log("5:", 99 + 1);
+// 6
+function subtract() {
+    return 100 - 45;
+}
+console.log("6:", subtract());
+// 7
+var walletBalance = 500;
+walletBalance = walletBalance - 120;
+console.log("7:", walletBalance);
+// 8
+var a = 2000;
+var b = 850;
+console.log("8:", a - b);
+// 9
+var stock = 50;
+stock -= 5;
+console.log("9:", stock);
+// 10
+console.log("10:", 150 - 75);
+// 11
+function multiplyByTen(n) {
+    return n * 10;
+}
+console.log("11:", multiplyByTen(5));
+// 12
+var totalPrice = 5 * 15;
+console.log("12:", totalPrice);
+// 13
+console.log("13:", 12 * 12);
+// 14
+// let lenght: number = 20;
+// let width: number = 10;
+// console.log("14:", length * width);
+// 15
+var value = 30;
+console.log("15:", value * 3);
+// 16
+function divide() {
+    return 100 / 4;
+}
+console.log("16:", divide());
+// 17
+console.log("17:", 50 / 5);
+// 18
+var half = 1500 / 2;
+console.log("18:", half);
+// 19
+console.log("19:", 81 / 9);
+// 20
+console.log("20:", 10 % 3);
