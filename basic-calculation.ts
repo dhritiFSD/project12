e409f333a141b171b0f7a574efa07ce2dc550d0d
Gajjar Dhriti <dhriti.g1012@gmail.com>
2026-03-02 11:50:17 +0530

    Add files via upload

---
+++ basic-calculation.ts
@@ -0,0 +1,87 @@
+function addTwoNumbers(a: number, b: number): number {
+    return a + b;
+}
+console.log("1:", addTwoNumbers(25, 35));
+
+// 2
+let num1: number = 10;
+let num2: number = 20;
+let num3: number = 30;
+console.log("2:", num1 + num2 + num3);
+
+// 3
+let grandTotal: number = 150 + 250;
+console.log("3:", grandTotal);
+
+// 4
+let score: number = 80;
+score = score + 10;
+console.log("4:", score);
+
+// 5
+console.log("5:", 99 + 1);
+
+// 6
+function subtract(): number {
+    return 100 - 45;
+}
+console.log("6:", subtract());
+
+// 7
+let walletBalance: number = 500;
+walletBalance = walletBalance - 120;
+console.log("7:", walletBalance);
+
+// 8
+let a: number = 2000;
+let b: number = 850;
+console.log("8:", a - b);
+
+// 9
+let stock: number = 50;
+stock -= 5;
+console.log("9:", stock);
+
+// 10
+console.log("10:", 150 - 75);
+
+// 11
+function multiplyByTen(n: number): number {
+    return n * 10;
+}
+console.log("11:", multiplyByTen(5));
+
+// 12
+let totalPrice: number = 5 * 15;
+console.log("12:", totalPrice);
+
+// 13
+console.log("13:", 12 * 12);
+
+// 14
+// let length: number = 20;
+// let width: number = 10;
+// console.log("14:", length * width);
+
+// 15
+let value: number = 30;
+console.log("15:", value * 3);
+
+// 16
+function divide(): number {
+    return 100 / 4;
+}
+console.log("16:", divide());
+
+// 17
+console.log("17:", 50 / 5);
+
+// 18
+let half: number = 1500 / 2;
+console.log("18:", half);
+
+// 19
+console.log("19:", 81 / 9);
+
+// 20
+console.log("20:", 10 % 3);
